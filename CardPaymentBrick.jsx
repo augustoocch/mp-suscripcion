0afbbdcedf9449cc4d3da295cb046f6a739b093d
Augusto Occhiuzzi <96669705+augustoocch@users.noreply.github.com>
2024-09-14 16:14:05 -0300

    Update CardPaymentBrick.jsx

--- CardPaymentBrick.jsx
+++ CardPaymentBrick.jsx
@@ -8,91 +8,57 @@ const MP_PUB_KEY = process.env.NEXT_PUBLIC_MP_PUB_KEY;
 ** Solo manda al BE para generar la suscripcion
 */
 
-function CardPaymentBrick({ suscription, planId, userLoged, onExecution }) {
-  const initialFetchRef = useRef(false);
+
+function CardPaymentBrickReact({ suscription, planId, userLoged, onExecution }) {
 
   const handleApprobeReject = async (status, paymentId) => {
     onExecution(status, paymentId);
   };
 
   useEffect(() => {
-    if (!initialFetchRef.current) {
-      initialFetchRef.current = true;
-      const loadMercadoPago = async () => {
-        if (!window.MercadoPago) {
-          const script = document.createElement('script');
-          script.src = 'https://sdk.mercadopago.com/js/v2';
-          script.async = true;
-          script.onload = () => {
-            createPaymentBrick();
-          };
-          document.body.appendChild(script);
-        } else {
-          createPaymentBrick();
-        }
-      };
+    initMercadoPago(MP_PUB_KEY);
+  }, []);
 
-      const createPaymentBrick = async () => {
-        const mp = new MercadoPago(MP_PUB_KEY, { locale: 'es-AR' });
-        const bricksBuilder = mp.bricks();
-        const settings = {
-          initialization: {
-            amount: suscription.amount,
-            payer: {
-              firstName: userLoged.name,
-              lastName: userLoged.surname,
-              email: userLoged.auth.email,
-            },
-          },
-          customization: {
-            visual: {
-              style: {
-                customVariables: {
-                  theme: 'default',
-                }
-              }
-            },
-            paymentMethods: {
-              maxInstallments: 1,
-            }
-          },
-          callbacks: {
-            onReady: async () => {
-            },
-            onSubmit: async (cardFormData) => {
-              try {
-                const userString = JSON.stringify(userLoged);
-                const approbe = await saveSuscriptionPreaproval(suscription.amount, planId, userString, cardFormData);
-                if (approbe.status !== 200) {
-                  throw new Error('Error al aprobar la suscripción');
-                }
-                handleApprobeReject(true, approbe.data.id);
-              } catch (error) {
-                console.log('Error en la transacción', error);
-                handleApprobeReject(false, null);
-                return false;
-              }
-            },
-            onError: (error) => {
-              console.log('Error en la transacción', error);
-              handleApprobeReject(false, null);
-              return false;
-            },
-          },
-        };
-        window.cardPaymentBrickController = await bricksBuilder.create('cardPayment', 'cardPaymentBrick_container', settings);
-      };
+  const initialization = {
+    amount: suscription.amount,
+  };
 
-      const initialize = async () => {
-        await loadMercadoPago();
-      };
-
-      initialize();
+  const onSubmit = async (formData) => {
+    try {
+      const userString = JSON.stringify(userLoged);
+      const approbe = await saveSuscriptionPreaproval(suscription.amount, planId, userString, formData);
+      if (approbe.status !== 200) {
+        throw new Error('Error al aprobar la suscripción');
+      }
+      handleApprobeReject(true, approbe.data.id);
+    } catch (error) {
+      console.log('Error en la transacción', error);
+      handleApprobeReject(false, null);
+      return false;
     }
-  }, [planId, userLoged]);
+  };
 
-  return <div id="cardPaymentBrick_container"></div>;
+  const onError = (error) => {
+    handleApprobeReject(false, null);
+      return false;
+  };
+
+  // Función que se llama cuando el Brick está listo
+  const onReady = () => {
+    console.log('Brick listo');
+  };
+
+  return (
+    <div>
+      <CardPayment
+        initialization={initialization}
+        onSubmit={onSubmit}
+        onReady={onReady}
+        onError={onError}
+      />
+    </div>
+  );
 };
 
-export default CardPaymentBrick;
+export default CardPaymentBrickReact;
 
